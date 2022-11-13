aa13f2c250db1d4377cf070a0adbe35961745461
Dorian Tsai <dorian.tsai@gmail.com>
2022-11-13 16:12:05 +1000

    approximate tank and spawn calculations

--- coral_spawn_counter/plot_spawn_table.py
+++ coral_spawn_counter/plot_spawn_table.py
@@ -32,5 +32,20 @@ avg_count = np.mean(counts)
 std_count = np.std(counts)
 print(f'For {host}:, mean count: {avg_count}, std_dev: {std_count}')
 
+
+area_cslics = 3.0 # cm^2
+rad_tank = 105.0/2 # cm^2
+area_tank = np.pi * rad_tank**2
+manual_count = 420000 # spawn
+
+nimage_to_tank = area_tank / area_cslics
+
+expected_count_perimage = manual_count / nimage_to_tank
+print(f'Expected count per image = {expected_count_perimage}')
+
+cslics_estimate_tank = avg_count * nimage_to_tank 
+print(f'Manual count for entire tank = {manual_count}')
+print(f'Expected range (+- 10%): min {manual_count*0.9}, max {manual_count * 1.1}')
+print(f'Cslics spawn count for entire tank = {cslics_estimate_tank}')
 # import code
 # code.interact(local=dict(globals(), **locals()))
