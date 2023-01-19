0fe0a8a4bfa25d034e96c8d8ee746d32a5a7dd64
Dorian Tsai <dorian.tsai@gmail.com>
2023-01-19 10:58:59 +1000

    quick script to rename images by putting cslicsid at front of image name

---
+++ coral_spawn_counter/rename_images.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+
+# rename images by adding cslics_id to front of image names
+
+import os
+import shutil
+
+# TODO: should update camera trigger code to add cslics_id to image name
+
+# get cslics id
+# get list of images in cslics images folder
+# rename all images
+# repeat for each cslics id within folder name
+
+# dataset folder name
+data_dir = '/home/dorian/Data/acropora_maggie_tenuis_dataset_100/20221114_AMaggieTenuis'
+print(data_dir)
+
+# get all cslics within given dataset
+cslics_ids = os.listdir(data_dir)
+print(cslics_ids)
+
+for cs in cslics_ids:
+    
+    img_dir = 'images_reduced'
+    img_path = os.path.join(data_dir, cs, img_dir)
+    
+    img_names = os.listdir(img_path)
+    
+    for name in img_names:
+        # assume .png at end of name, which we first remove
+        # then append _cslics##.png
+        new_name = cs + '_' + name
+        shutil.move(os.path.join(img_path, name), 
+                    os.path.join(img_path, new_name))
+        
+print('done')
