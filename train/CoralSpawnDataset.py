4967bdd4129b36a5bc4f4829df5a666a5367fbe2
Dorian Tsai <dorian.tsai@gmail.com>
2023-01-20 19:19:22 +1000

    work in progress CoralSpawnDataset.py dataset object for training

--- train/CoralSpawnDataset.py
+++ train/CoralSpawnDataset.py
@@ -1,8 +1,32 @@
 #! /usr/bin/env python3
 
+"""
+CoralSpawnDataset object for training the coral spawn and larvae counter    
+"""
+
 # TODO CoralSpawnDataset 
 # inherit from Annotations.py
 # init
 # get_item
 # transforms
 # len
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from annotation import Annotations
+
+class CoralSpawnDataset(Dataset):
+    def __init__(self, ann_file: str, img_dir: str):
+
+        self.ann_file = ann_file # absolute filepath
+        self.img_dir = img_dir # absolute filepath to image directory
+        Ann = Annotations(self.ann_file, self.img_dir)
+        self.annotations = Ann.annotations
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, index):
+        return self.data_list[index]
+
+
