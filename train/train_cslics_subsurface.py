273034a426a432c60d281192b3e9db920e612f0e
CaptainReginald <Rwardleworth@outlook.com>
2025-04-02 16:29:21 +1000

    HPC code/ updates

--- train/train_cslics_subsurface.py
+++ train/train_cslics_subsurface.py
@@ -1,20 +1,45 @@
+#! /usr/bin/env python3
+
+"""trian_segmenter.py
+train basic yolov8 model for image segmentation
+"""
+
 from ultralytics import YOLO
 import torch
 
-# load pretrained model
-model = YOLO('weights/yolov8n.pt')
+data_file = '/mnt/hpccs01/home/wardlewo/Data/cslics_subsurface_data/cslics_2023_2024_subsurface_M12.yaml'
+#data_file = sys.argv[1]
 
+# load model
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-# train the model
-model.train(data='../data/side_imaging.yml', 
-            epochs=200, 
-            imgsz=1280,
-            workers=4,
-            cache=True,
-            amp=False,
-            batch=4,
-            ).to(device)
+model = YOLO("yolov8n.pt")
+#model = YOLO("/home/java/Java/cslics/resolution_test_results/models/resolution_test_640/weights/Cslic_640_best.pt")
+model.info()
 
 
-print('done')
+#model.train(data=data_file, epochs=200, batch=10)
+
+# classes arg is lightweight and simply ignore classes that are not included in the classes list, 
+# during train, val and predict, it has no effect on model architecture.
+model.train(data=data_file, 
+            device      = 0,       #For HPC, set to 0 or delete otherwise      
+            epochs      = 500, 
+            batch       = 16,  
+            project     = "cslic_subsurface_20250205",
+            workers     = 4,
+            patience    = 50,
+            pretrained  = False,
+            save        = True,
+            save_period = 25,
+            deterministic = False,
+            imgsz       = 640,
+            #Augmentation
+            #HSV added via Albumentations
+            scale       = 0.2,
+            flipud      = 0.5,
+            fliplr      = 0.5
+            ) #test run
+
+# # for interactive debugger in terminal:
+# import code
+# code.interact(local=dict(globals(), **locals()))
